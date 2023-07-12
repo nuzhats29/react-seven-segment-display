8fc0c0165f8d55dda6984a6a05b890c1b8832f1a
nuzhats29 <119833468+nuzhats29@users.noreply.github.com>
2023-07-12 18:43:56 +0530

    Update Arrow.js

--- src/Arrow.js
+++ src/Arrow.js
@@ -1,29 +1,10 @@
 import React from 'react';
 
 const INVERSE_DIRECTION = {
-  left: 'right',
-  right: 'left',
-  top: 'bottom',
-  bottom: 'top'
-};
-
-const BORDER_COLOR_PROP = {
-  left: 'borderLeftColor',
-  right: 'borderRightColor',
-  top: 'borderTopColor',
   bottom: 'borderBottomColor'
 };
 
-export default function ({ size, color, direction }) {
-  const style = {
-    position: 'absolute',
-    width: '0px',
-    height: '0px',
-    borderColor: 'transparent',
-    borderWidth: `${size * 0.5}px`,
-    borderStyle: 'solid'
-  };
-
+export default function ({ size
   const direction_inv = INVERSE_DIRECTION[direction];
 
   style[BORDER_COLOR_PROP[direction_inv]] = color;
