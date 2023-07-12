622053833ac573cab98ec0a26978805ae40937bf
nuzhats29 <119833468+nuzhats29@users.noreply.github.com>
2023-07-12 19:04:43 +0530

    Update index.js
    test

--- dev/index.js
+++ dev/index.js
@@ -7,7 +7,9 @@ document.body.appendChild(root);
 
 function Digits() {
   const arr = [];
-
+let array = [1, 2, 3];
+array.slice("not a number");
+  eval()
   for (let i = 0; i <= 9; i += 1) {
     arr.push(<SevenSegmentDisplay style={{ marginRight: '5px' }} value={i} key={i} width={40} height={10} />);
   }
